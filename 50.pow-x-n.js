a7623b4d9da054ef6c79efa9c8766833947cc208
buraktnv <burk3912@gmail.com>
2022-04-29 11:34:54 +0300

    .

---
+++ 50.pow-x-n.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=50 lang=javascript
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = (x, n) => (x ** n).toFixed(5);
+
+// @lc code=end
