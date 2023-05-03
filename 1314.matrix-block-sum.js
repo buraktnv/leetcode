1a860cb5a45bebc87ef8af7dbf87748e7159a53a
buraktnv <buraktnv@gmail.com>
2023-05-03 03:45:22 +0300

    .

---
+++ 1314.matrix-block-sum.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1314 lang=javascript
+ *
+ * [1314] Matrix Block Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[][]}
+ */
+var matrixBlockSum = function (mat, k) {
+  sadfsdfsdf;
+};
+// @lc code=end
