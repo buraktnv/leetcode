ade488bd904d5cf08943bca0d25d87ba28e94e5c
buraktnv <buraktnv@gmail.com>
2023-06-04 02:44:47 +0300

    Create 2.add-two-numbers.js

---
+++ 2.add-two-numbers.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let num1 = "",
+    num2 = "";
+
+  while (l1) {
+    num1 = l1.val + num1;
+    l1 = l1.next;
+  }
+
+  while (l2) {
+    num2 = l2.val + num2;
+    l2 = l2.next;
+  }
+
+  const sum = (BigInt(num1) + BigInt(num2)).toString();
+
+  let list = null;
+  let i = 0;
+  while (i < sum.length) {
+    list = new ListNode(sum[i], list);
+    i++;
+  }
+
+  return list;
+};
+// @lc code=end
