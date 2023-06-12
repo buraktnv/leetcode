e10114c47917c89179b90e11dd411d0a597f38fb
buraktnv <buraktnv@gmail.com>
2023-06-12 22:03:19 +0300

    Create 121.best-time-to-buy-and-sell-stock.js

---
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=121 lang=javascript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+  for (const price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    } else if (price - minPrice > maxProfit) {
+      maxProfit = price - minPrice;
+    }
+  }
+  return maxProfit;
+};
+// @lc code=end
