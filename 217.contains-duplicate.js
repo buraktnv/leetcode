345233599b3e4446acf052ff055cfcd20dba8bc1
buraktnv <buraktnv@gmail.com>
2023-06-17 10:52:04 +0300

    Create 217.contains-duplicate.js

---
+++ 217.contains-duplicate.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=217 lang=javascript
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  return Array.from(new Set(nums)).join`` !== nums.join``;
+};
+// @lc code=end
