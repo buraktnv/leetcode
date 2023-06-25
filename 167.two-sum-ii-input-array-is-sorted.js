1b36ac560f263c6d400b37e77bf04d24d4fd6be2
buraktnv <buraktnv@gmail.com>
2023-06-25 03:11:01 +0300

    Update 167.two-sum-ii-input-array-is-sorted.js

--- 167.two-sum-ii-input-array-is-sorted.js
+++ 167.two-sum-ii-input-array-is-sorted.js
@@ -11,17 +11,14 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-  let p1 = 0;
-  let p2 = numbers.length - 1;
-
-  while (numbers[p1] + numbers[p2] !== target) {
-    if (numbers[p1] + numbers[p2] > target) {
-      p2--;
-    } else {
-      p1++;
-    }
+  let p1 = 0,
+    p2 = numbers.length - 1,
+    t = numbers[p1] + numbers[p2];
+  while (t - target != 0) {
+    t - target > 0 ? p2-- : p1++;
+    t = numbers[p1] + numbers[p2];
   }
 
-  return [p1 + 1, p2 + 1];
+  return [++p1, ++p2];
 };
 // @lc code=end
