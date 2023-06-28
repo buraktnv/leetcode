0cc698c3e07891750b404dd25ec420c4c44bc271
buraktnv <buraktnv@gmail.com>
2023-06-29 00:03:05 +0300

    Create 94.binary-tree-inorder-traversal.js

---
+++ 94.binary-tree-inorder-traversal.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  if (root === null) return [];
+  return [
+    ...inorderTraversal(root.left),
+    root.val,
+    ...inorderTraversal(root.right),
+  ];
+};
+// @lc code=end
