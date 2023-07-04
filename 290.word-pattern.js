996b13825396d19e6f995c06641aa30385fddc83
buraktnv <buraktnv@gmail.com>
2023-07-04 15:55:53 +0300

    Create 290.word-pattern.js

---
+++ 290.word-pattern.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=290 lang=javascript
+ *
+ * [290] Word Pattern
+ */
+
+// @lc code=start
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+
+  s = s.split` `;
+  for(let i = 0;i < s.length;i++){
+    
+  }
+};
+// @lc code=end
+
