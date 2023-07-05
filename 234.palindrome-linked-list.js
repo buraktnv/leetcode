f312554be0cd36a845594f618f3950af01d658bc
buraktnv <buraktnv@gmail.com>
2023-07-05 17:17:19 +0300

    Create 234.palindrome-linked-list.js

---
+++ 234.palindrome-linked-list.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=234 lang=javascript
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  let str = "";
+  while (head) {
+    str += head.val;
+    head = head.next;
+  }
+  if (str === str.split``.reverse``.join``) return true;
+  return false;
+};
+// @lc code=end
