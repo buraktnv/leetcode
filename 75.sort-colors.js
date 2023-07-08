7a1ef7809d88cb9458d758492d6130991654f455
buraktnv <buraktnv@gmail.com>
2023-07-09 00:44:12 +0300

    Create 75.sort-colors.js

---
+++ 75.sort-colors.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=75 lang=javascript
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  return nums.sort((a, b) => a - b);
+};
+// @lc code=end
